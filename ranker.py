8a30a1a67b3f1757068d7fb2960f7643b87bdaee
Yu Liu(Relevance) <yliu8@yliu8-ld1.linkedin.biz>
2019-03-06 15:25:20 -0800

    using batch size for ltr

--- ranker.py
+++ ranker.py
@@ -30,6 +30,7 @@ import bert.optimization as optimization
 import bert.tokenization as tokenization
 import tensorflow as tf
 import numpy
+from ranking.tensorflow_ranking.python.data import parse_from_sequence_example
 from generate_data import get_example
 from tqdm import tqdm
 
@@ -88,11 +89,11 @@ flags.DEFINE_bool(
     "do_predict", False,
     "Whether to run the model in inference mode on the test set.")
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+flags.DEFINE_integer("train_batch_size", 12, "Total batch size for training. Note: batch sizes are used for query level training.")
 
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+flags.DEFINE_integer("eval_batch_size", 12, "Total batch size for eval. Note: batch sizes are used for query level training.")
 
-flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
+flags.DEFINE_integer("predict_batch_size", 12, "Total batch size for predict. Note: batch sizes are used for query level training.")
 
 flags.DEFINE_float("learning_rate", 5e-5,
                    "The initial learning rate for Adam.")
@@ -559,6 +560,7 @@ def file_based_input_fn_builder(input_file, seq_length, is_training,
                                 drop_remainder):
   """Creates an `input_fn` closure to be passed to TPUEstimator."""
 
+
   name_to_features = {
       "input_ids": tf.FixedLenFeature([seq_length], tf.int64),
       "input_mask": tf.FixedLenFeature([seq_length], tf.int64),
@@ -584,7 +586,6 @@ def file_based_input_fn_builder(input_file, seq_length, is_training,
     "query": tf.FixedLenSequenceFeature([], tf.string),
     "qcnt": tf.FixedLenSequenceFeature([], tf.int64),
   }
-
   def _decode_record(record, name_to_features):
     """Decodes a record to a TensorFlow example."""
     example = tf.parse_single_example(record, name_to_features)
@@ -599,34 +600,24 @@ def file_based_input_fn_builder(input_file, seq_length, is_training,
 
     return example
 
-  def _extract_examples(record): 
-    print(record)
-    print(record.__dict__)
-    context, sequence = tf.parse_single_sequence_example(record, 
-      context_features = name_to_context_features,
-      sequence_features = name_to_sequence_features)
-    print(context)
-    print(sequence)
-    leng = context["len"]
-    print(leng)
-    print(tf.unstack(leng))
-    return []
+  def _extract_examples(record, batch_size): 
+    return parse_from_sequence_example(
+      record,
+      batch_size,
+      context_feature_spec=name_to_context_features,
+      example_feature_spec=name_to_sequence_features
+    )
 
   def input_fn(params):
     batch_size = params["batch_size"]
     # For training, we want a lot of parallel reading and shuffling.
     # For eval, we want no shuffling and parallel reading doesn't matter.
     d = tf.data.TFRecordDataset(input_file)
-    d = d.map( _extract_examples )
-    # d = d.flat_map( lambda x: tf.data.Dataset().from_tensor_slices(x) )
-    # if is_training:
-    #   d = d.repeat()
-    #   d = d.shuffle(buffer_size=100)
-    # d = d.apply(
-    #     tf.contrib.data.map_and_batch(
-    #         lambda record: _decode_record(record, name_to_features),
-    #         batch_size=batch_size,
-    #         drop_remainder=drop_remainder))
+    d = d.map( lambda x : _extract_examples(x, batch_size) )
+    if is_training:
+      d = d.repeat()
+      d = d.shuffle(buffer_size=100)
+    # do NOT batch afterwards
     return d
 
   return input_fn
